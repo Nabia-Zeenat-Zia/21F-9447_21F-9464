4ce0dc004b1287363785b2fd4e72b3dae18ba6c0
[Aqsa Javaid] <[aqsajavaid185@gmail.com]>
2023-10-06 22:56:42 +0500

    Added Function div and called it in main

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,3 @@
-package lab7;
 
 public class Calculator {
 
@@ -9,8 +8,23 @@ public class Calculator {
 	public static int sub(int num1, int num2) {
 		return num1-num2;
 	}
-	
 	public static int mul(int num1, int num2) {
 		return num1*num2;
+	}
+	public static int div(int num1, int num2) {
+		return num1/num2;
+	}
+	
+}
+class main
+{
+
+	public static void main(String[] args) {
+		
+		Calculator.add(2,4);
+		Calculator.sub(5,3);
+		Calculator.mul(3, 2);
+		Calculator.div(10, 2);
+
 	}
 }
